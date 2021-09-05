85b9917c77133603a555f9e78045fe324bca70e0
EDostieSlavich <edostie@umass.edu>
2021-09-05 10:21:22 -0400

    question1.py

--- question1.py
+++ question1.py
@@ -9,35 +9,26 @@ class Hw1Q1():
 
     def timeConvert(self, input_seconds):
         """Input number of seconds to convert to # day(s) # hour(s) # minute(s) # second(s)"""
+        condensed = {}
         numDays = input_seconds/self.day                                    # calculate number of days
         numHours = (input_seconds - int(numDays)*self.day)/self.hour        # "                 " hours
         numMin = (numHours*self.hour - int(numHours)*self.hour)/self.minute # "                 " minutes
-        numSec = (numMin*self.minute - int(numMin)*self.minute)/self.second # "                 " seconds
-        if int(numDays) == 1:                                               # change each quantity to string
-            strDays = str(int(numDays))+' day '                             # or remove if quantity is 0
-        elif int(numDays) > 1:
-            strDays = str(int(numDays))+' days '
-        else:
-            strDays = ''
-        if int(numHours) == 1:
-            strHours = str(int(numHours))+' hour '
-        elif int(numHours) > 1:
-            strHours = str(int(numHours))+' hours '
-        else:
-            strHours = ''
-        if int(numMin) == 1:
-            strMin = str(int(numMin))+' minute '
-        elif int(numMin) > 1:
-            strMin = str(int(numMin))+' minutes '
-        else:
-            strMin = ''
-        if int(numSec) == 1:
-            strSec = str(int(numSec))+' second '
-        elif int(numSec) > 1:
-            strSec = str(int(numSec))+' seconds '
-        else:
-            strSec = ''
-        return print(strDays+strHours+strMin+strSec)                        # return new format
+        numSec = (numMin*self.minute - int(numMin)*self.minute)/self.second # "                 " seconds))
+        convert = {"days": int(numDays),                                    # dictionary of time keys and values
+                   "hours": int(numHours),
+                   "minutes": int(numMin),
+                   "seconds": int(numSec)
+                   }
+        for x, y in convert.items():                                        # remove keys and values that are zero
+            if y > 0:
+                condensed.update({x: y})
+                if y == 1:                                                  # update plural keys if necessary
+                    condensed[x.strip('s')] = condensed[x]
+                    del condensed[x]
+        printedForm = ''
+        for x, y in condensed.items():                                      # formatting print style
+            printedForm = printedForm+str(y)+" "+x+" "
+        return print(printedForm)
 
 
 if __name__ == '__main__':
